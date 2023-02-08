f30dc001528b4117ca61e90afc96a09322e8eda7
Alex Hazniuk <ohazniuk@gmail.com>
2023-02-08 12:44:31 +0200

    generate RestoreFile

---
+++ src/com/company/RestoreFile.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class RestoreFile {
+    private String name;
+
+    public RestoreFile(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
